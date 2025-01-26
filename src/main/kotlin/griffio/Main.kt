83c92d66d2cb870f60b6812dc884b3f21575f77d
griff і⊙ <346896+griffio@users.noreply.github.com>
2025-01-26 19:00:54 +0000

    Update Main.kt

--- src/main/kotlin/griffio/Main.kt
+++ src/main/kotlin/griffio/Main.kt
@@ -4,13 +4,12 @@ import app.cash.sqldelight.dialect.api.SqlDelightModule
 import app.cash.sqldelight.dialects.postgresql.PostgreSqlDialect
 import app.cash.sqldelight.driver.jdbc.asJdbcDriver
 import org.postgresql.ds.PGSimpleDataSource
-import java.util.ServiceLoader
 
 private fun getSqlDriver() = PGSimpleDataSource().apply {
-    setURL("jdbc:postgresql://localhost:5432/srs")
+    setURL("jdbc:postgresql://localhost:5432/geo")
     applicationName = "App Main"
-    user = "srs"
-    password = "srs"
+    user = "geo"
+    password = "geo"
 }.asJdbcDriver()
 
 fun main() {
